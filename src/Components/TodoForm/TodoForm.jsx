31883f3894c58135c21aa46ede16ec53ad6677f3
SandeSydney <sandesydney01@gmail.com>
2022-10-28 07:20:55 +0300

    Edit functionality to todo form

--- src/Components/TodoForm/TodoForm.jsx
+++ src/Components/TodoForm/TodoForm.jsx
@@ -1,11 +1,18 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import './todoform.css'
 
 function TodoForm(props) {
-    const [input, setInput] = useState('')
+    // check if an edit has been passed
+    const [input, setInput] = useState(props.edit ? props.edit.value : '')
 
     const priorityRef = useRef()
+    const inputRef = useRef(null)
+
+    // onPage load
+    useEffect(() => {
+        inputRef.current.focus()
+    })
 
     const handleChange = (e) => {
         setInput(e.target.value)
@@ -18,7 +25,7 @@ function TodoForm(props) {
             id: uuidv4(),
             title: input,
             priority: priorityRef.current.value,
-            complete:false
+            complete: false
         })
 
         setInput('')
@@ -26,26 +33,55 @@ function TodoForm(props) {
 
     return (
         <form className='todo-form' onSubmit={handleSubmit}>
-            <div className='formElement'>
-                <label htmlFor="title">Title:</label>
-                <input
-                    type="text"
-                    placeholder='Add Todo'
-                    value={input}
-                    name='title'
-                    className='todo-input'
-                    onChange={handleChange}
-                />
-            </div>
-            <div className='formElement'>
-                <label htmlFor="priority">Priority:</label>
-                <select name="priority" id="priority" ref={priorityRef}>
-                    <option value="high" >High</option>
-                    <option value="medium">Medium</option>
-                    <option value="low">Low</option>
-                </select>
-            </div>
-            <button className='todo-button' id='addTodo'>Add Todo</button>
+            {props.edit ? (
+                <>
+                    <div className='formElement'>
+                        <label htmlFor="title">Title:</label>
+                        <input
+                            type="text"
+                            placeholder='Edit todo'
+                            value={input}
+                            name='title'
+                            className='todo-input'
+                            onChange={handleChange}
+                            ref={inputRef}
+                        />
+                    </div>
+                    <div className='formElement'>
+                        <label htmlFor="priority">Priority:</label>
+                        <select name="priority" id="priority" ref={priorityRef}>
+                            <option value="high" >High</option>
+                            <option value="medium">Medium</option>
+                            <option value="low">Low</option>
+                        </select>
+                    </div>
+                    <button className='todo-button' id='editTodo'>Update</button>
+                </>
+            ) : (
+                <>
+                    <div className='formElement'>
+                        <label htmlFor="title">Title:</label>
+                        <input
+                            type="text"
+                            placeholder='Add Todo'
+                            value={input}
+                            name='title'
+                            className='todo-input'
+                            onChange={handleChange}
+                            ref={inputRef}
+                        />
+                    </div>
+                    <div className='formElement'>
+                        <label htmlFor="priority">Priority:</label>
+                        <select name="priority" id="priority" ref={priorityRef}>
+                            <option value="high" >High</option>
+                            <option value="medium">Medium</option>
+                            <option value="low">Low</option>
+                        </select>
+                    </div>
+                    <button className='todo-button' id='addTodo'>Add Todo</button>
+                </>
+            )}
         </form>
     )
 }
